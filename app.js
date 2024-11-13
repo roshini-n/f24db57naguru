cc9971b591885cbba0f53c78b0cd43a18f27de99
roshini-n <S574380@nwmissouri.edu>
2024-11-13 09:30:52 -0600

    changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if (!connectionString) {
   console.error('MongoDB is not set!');
   process.exit(1);  // Exit the process if URI is not set
 }
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000,  // Shorter timeout
+mongoose.connect(connectionString, { serverSelectionTimeoutMS: 5000,
   socketTimeoutMS: 45000 })
 .then(() => console.log("MongoDB connected successfully"))
 .catch((err) => console.error("MongoDB connection error:", err));
--- models/relic.js
+++ models/relic.js
@@ -1,11 +1,10 @@
-// models/relic.js
+
 const mongoose = require('mongoose');
 
-const relicSchema = mongoose.Schema({
+const relicSchema = new mongoose.Schema({
     relic_name: { type: String, required: true },
     origin: { type: String, required: true },
     estimated_value: { type: Number, required: true }
 });
-
-module.exports = mongoose.model('Relic', relicSchema);
+const Relic = mongoose.model('Relic', relicSchema);
 module.exports = Relic;
