73228a8463854a7bc1f85604c2c64f87424dacaf
roshini-n <S574380@nwmissouri.edu>
2024-11-13 16:05:03 -0600

    committed done

--- controllers/relicController.js
+++ controllers/relicController.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 // controllers/relicController.js
 var Relic = require('../models/relic');
 
--- routes/relics.js
+++ routes/relics.js
@@ -1,13 +1,14 @@
 var express = require('express');
-const relic_controller = require('../controllers/relicController');  // Corrected name
 var router = express.Router();
+const relic_controller = require('../controllers/relicController');
+
 
 // GET request to fetch all relics
 router.get('/', relic_controller.relic_view_all_Page);
-
-// POST request to create a new relic
 router.post('/relics', relic_controller.relic_create_post);
-
+router.get('/:id', relic_controller.relic_detail);
+router.put('/:id', relic_controller.relic_update_put);
+router.delete('/:id', relic_controller.relic_delete);
 // GET /resource/:id - Get details of a specific relic by ID
 // router.get('/:id', relic_controller.relic_detail);
 
